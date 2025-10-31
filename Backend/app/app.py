ca1d69600d3642209206650ea97299b11f73e7c1
Parth Tyagi <parthtyagi1204@gmail.com>
2025-10-31 01:52:23 -0400

    started o backend

---
+++ Backend/app/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title = "Cylytic Backend",
+    version = "1.0.0",
+    description="AI-powered period and wellness tracker backend"
+)
+
+@app.get("/")
+def root():
+    return {"message": "Cyclytic backend running"}
